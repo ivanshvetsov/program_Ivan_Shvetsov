fa4ab7bcaabf7059a8771dfde77376fb334c90d1
ivanshvetsov <154842696+ivanshvetsov@users.noreply.github.com>
2023-12-28 13:47:12 +0300

    Update 4_o.py

--- 4_o.py
+++ 4_o.py
@@ -1,10 +1,12 @@
-A = int(input("Введите значение A: "))
-B = int(input("Введите значение B (A < B): "))
-
-sum_all = sum(range(A, B + 1))
-sum_even = sum(range(A, B + 1, 2))
-sum_odd = sum(range(A + 1, B + 1, 2))
-
-print(f"Сумма всех чисел от {A} до {B}: {sum_all}")
-print(f"Сумма четных чисел от {A} до {B}: {sum_even}")
-print(f"Сумма нечетных чисел от {A} до {B}: {sum_odd}")
+a = int(input('Введите целое число А (A<B)'))
+b = int(input("Введите целое число B "))
+all = 0
+even = 0
+odd = 0
+for i in range(a,b+1):
+    all += i
+    if i%2 == 0:
+        even += i
+    else:
+        odd += i
+print('Общаая сумма =',all,"Сумма четных чисел =",even,"Сумма нечетных чисел =",odd)
