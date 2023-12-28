daeea32332ab3940c2e84a20565e558c7e1c4d2c
ivanshvetsov <154842696+ivanshvetsov@users.noreply.github.com>
2023-12-28 15:57:03 +0300

    Update 7_o.py

--- 7_o.py
+++ 7_o.py
@@ -1,17 +1,16 @@
-import sys
+A = int(input("введите сторону A: " ))
+B = int(input("введите сторону B: " ))
+C = int(input("введите сторону C: " ))
+ramen = 0
+CvA = 0
+Cvb = 0
+while A>= C:
+    A-= C
+    CvA += 1
 
-a = float(input('Введите сторону прямоугольника А '))
-b = float(input('Введите сторону прямоугольника В '))
-c = float(input('Введите число С '))
-if a<=0 or b<=0 or c<=0:
-    print('Ошибка. Число должно быть больше нуля.')
-    sys.exit()
-
-priamoug = a * b
-kvadr = c ** 2
-summ = priamoug
-done = 0
-while summ>= kvadr:
-    summ -= kvadr
-    done += 1
-print('В прямоугольник AB поместятся',done,'квадратa(ов) со стороной С')
+while B >= C:
+    B -= C
+    Cvb += 1
+for i in range(Cvb):
+    ramen += CvA
+print(ramen)
